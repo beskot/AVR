a23cfa326ded543fdc2a827b9cc79129db302da0
Evgenov Konstantin <eka5.88@mail.ru>
2021-09-06 15:53:56 +0300

    Добавлены комментарии Docstring

--- ModBusSimpleTests_py/MbClient.py
+++ ModBusSimpleTests_py/MbClient.py
@@ -1,40 +1,77 @@
 import serial
 import time
 
-'''Modbus client class'''
-
-
 class MbClient:
+    '''Класс modbus master (клиент)
+
+    Attributes
+    ----------
+    resp_bytes : list
+        массив байт, содержащий ответ ведомого устройства на запрос
+    __dev_addr : byte
+        Id ведомого устройства
+    '''
 
     def __init__(self, id):
         self.resp_bytes = []
         self.__dev_addr = id
 
-    '''Serail open'''
+    def connect(self, port, baudrate) -> None:
+        '''Метод для подключения к ведомому устройству по последовательному порту
 
-    def connect(self, port, boundrate):
-        self.__ser = serial.Serial(port, boundrate)
+        Parameters
+        ----------
+        port : int
+            Номер последовательного порта
+        baudrate : int
+            Символьная скорость
+        '''
+        self.__ser = serial.Serial(port, baudrate)
         time.sleep(3)
 
-    '''Serial close'''
+    def close(self) -> None:
+        '''Метод закрытия соединения'''
 
-    def close(self):
         if(self.__ser.isOpen()):
             self.__ser.close()
 
-    '''Modbus func 0x03'''
+    def read_h3(self, start, count) -> None:
+        '''Чтение состояния Holding Registers (Функция 03H)
 
-    def read_h3(self, start, count):
+        Parameters
+        ----------
+        start : int
+            Адрес регистра
+        count : int
+            Количество регистров
+        '''
+
         self.__execute([self.__dev_addr, 0x03, start >> 8, start &
                        0x00FF, count >> 8, count & 0x00FF])
 
-    '''Modbus func 0x10'''
+    def write_one_h10(self, start, data) -> None:
+        '''Установка одного Holding Register (Функция 10H)
 
-    def write_h10(self, start, data):
+        Parameters
+        ----------
+        start : int
+            Адрес регистра
+        data : int
+            Данные
+        '''
+
         self.__execute([self.__dev_addr, 0x10, start >> 8, start &
                         0x00FF, 0x00, 0x01, data >> 8, data & 0x00FF])
 
-    def __execute(self, buffer):
+    def __execute(self, buffer) -> None:
+        '''Закрытый метод для записи\чтения данных в ведомое устройство
+
+        Parameters
+        ----------
+        buffer : list
+            Запрос к ведомому устройству (в виде массива байт)
+        '''
+
         try:
             self.resp_bytes.clear()
             buffer.extend(self.__get_crc16(buffer))
@@ -43,12 +80,18 @@ class MbClient:
             while self.__ser.inWaiting() > 0:
                 self.resp_bytes.extend(self.__ser.read())
             print([hex(x) for x in self.resp_bytes])
-        except Exception:
-            print(f"Exeption: {Exception}")
+        except Exception as ex:
+            print(f"Exeption: {ex}")
 
-    '''Modbus CRC16'''
+    def __get_crc16(self, arraybyte) -> tuple:
+        '''Закрытый метод для расчета контрольной суммы
 
-    def __get_crc16(self, arraybyte):
+        Parameters
+        ----------
+        arraybyte : arraybyte
+            Запрос к ведомому устройству (в виде массива байт, но без контрольной суммы)
+        '''
+
         polynom = 0xa001  # полином modbus
         crcfull = 0xffff
         crcH, crcL = 0xff, 0xff
